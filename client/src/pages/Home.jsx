001db9fc87c768bc4f9e445f1594afeaea310d0a
kaysee <kayseeclintone@gmail.com>
2023-05-10 02:24:15 +0300

    notong serious

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ const Home = () => {
 		setLoading(true);
 
 		try {
-			const response = await fetch('http://localhost:5173/api/v1/post', {
+			const response = await fetch('https://snapai-6mpe.onrender.com', {
 				method: 'GET',
 				headers: {
 					'Content-Type': 'application/json',
