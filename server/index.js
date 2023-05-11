a01ca2038bd75f0c29e59870c5da62627496b4a5
kaysee <kayseeclintone@gmail.com>
2023-05-11 12:12:03 +0300

    fixed a bug in index.js backend code

--- server/index.js
+++ server/index.js
@@ -16,11 +16,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
 /* =====endpoints===== */
-app.use('/', (req, res) => {
-	/** testing route */
-	res.send('*****server running*****');
-});
-
 app.use('/api/v1/post', postRoutes);
 app.use('/api/v1/dalle', serverRoutes);
 
